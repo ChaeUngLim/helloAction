cff40f9e4a68059b4f67dd0a7029876d6db00138
ChaeUngLim <brad213@naver.com>
2024-07-03 09:47:46 +0900

    0703

---
+++ dockerimageTest/src/main/java/com/ohgiraffers/dockertest/DockerimageTestApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerimageTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerimageTestApplication.class, args);
+	}
+
+}
